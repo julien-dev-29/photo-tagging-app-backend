56af4a6c9df6ef6e0d7e50b85756232e9aa89071
jurol <jurol@example.com>
2025-10-20 12:51:37 +0200

    Update the controller

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,40 +1,83 @@
-import { Request, Response } from "express"
-const characters = [
+import { Request, Response } from "express";
+
+interface Character {
+    id: string;
+    name: string;
+    xMin: number;
+    xMax: number;
+    yMin: number;
+    yMax: number;
+    found: false
+}
+
+const characters: Character[] = [
     {
         id: crypto.randomUUID(),
-        name: "Jurol",
-        x: [250, 251, 252, 253, 254, 255],
-        y: [750, 751, 752, 753, 754, 755]
+        name: "Rick",
+        xMin: 440,
+        xMax: 585,
+        yMin: 330,
+        yMax: 545,
+        found: false
     },
     {
         id: crypto.randomUUID(),
-        name: "Maël",
-        x: [350, 351, 352, 353, 354, 355],
-        y: [150, 151, 152, 153, 154, 155]
+        name: "Morty",
+        xMin: 546,
+        xMax: 613,
+        yMin: 194,
+        yMax: 345,
+        found: false
     },
     {
         id: crypto.randomUUID(),
-        name: "Marie",
-        x: [850, 851, 852, 853, 854, 855],
-        y: [650, 651, 652, 653, 654, 655]
-    }
-]
+        name: "Jerry",
+        xMin: 650,
+        xMax: 705,
+        yMin: 50,
+        yMax: 220,
+        found: false
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Summer",
+        xMin: 745,
+        xMax: 807,
+        yMin: 465,
+        yMax: 604,
+        found: false
+    },
+    {
+        id: crypto.randomUUID(),
+        name: "Beth",
+        xMin: 857,
+        xMax: 910,
+        yMin: 218,
+        yMax: 350,
+        found: false
+    },
+];
+
 export default {
     get: async (req: Request, res: Response) => {
-        return res.json(characters)
+        return res.json(characters);
     },
-
     check: async (req: Request, res: Response) => {
         const { characterId, x, y } = req.body;
         const character = characters.find((c) => c.id === characterId);
         if (!character) {
             return res.status(404).json({ error: "Character not found" });
         }
-        const isValid = character.x.includes(Number(x)) && character.y.includes(Number(y));
+        // Vérification en O(1)
+        const isValid =
+            x >= character.xMin &&
+            x <= character.xMax &&
+            y >= character.yMin &&
+            y <= character.yMax;
         if (isValid) {
             return res.json({ message: "Success!" });
         } else {
             return res.json({ message: "Fail!" });
         }
     },
-}
+};
